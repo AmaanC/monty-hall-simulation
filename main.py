a7ce34611ef516e1e065bcb7d8b44c96be7a4ade
Amaan Cheval <amaan.cheval@gmail.com>
2015-07-11 11:07:22 +0530

    Made the code more flexible

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 
 import random
 
-total_doors = 3
-
+total_doors = 10
+num_doors_opened = 8 # How many doors does Monty open?
+trials = 10000
 
 got_car = 0
 got_goat = 0
@@ -22,12 +23,13 @@ def show_goat(car, chosen_door):
         doors.remove(car)
     if chosen_door in doors:
         doors.remove(chosen_door)
-    return random.choice(doors)
+    return random.sample(doors, num_doors_opened)
 
 def get_closed_doors(*args):
     doors = door_list[:]
     for arg in args:
-        doors.remove(arg)
+        if arg in doors:
+            doors.remove(arg)
     return doors
 
 def run_test():
@@ -36,8 +38,8 @@ def run_test():
     doors = door_list[:]
     car = place_car()
     chosen_door = pick_door()
-    open_door = show_goat(car, chosen_door)
-    doors_to_switch_to = get_closed_doors(chosen_door, open_door)
+    open_doors = show_goat(car, chosen_door)
+    doors_to_switch_to = get_closed_doors(chosen_door, *open_doors)
     chosen_door = random.choice(doors_to_switch_to)
 
     if chosen_door == car:
@@ -48,10 +50,13 @@ def run_test():
     # print('Got car: {}\nGot goat: {}'.format(got_car, got_goat))
 
 def main():
-    trials = 100000
     for _ in range(trials):
         run_test()
-    print('Cars win {} times in {} trials'.format(got_car / (got_car + got_goat), trials))
+    print(
+        ('Monty shows you {} goats out of {} doors.\n'
+        'Odds of getting the car is {}. Found running {} trials')
+        .format(num_doors_opened, total_doors, got_car / (got_car + got_goat), trials)
+    )
 
 if __name__ == '__main__':
     main()
